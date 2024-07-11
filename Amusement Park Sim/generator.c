6bbc3b24bce51f671099aef24cc533e7a5fe32eb
Talha101Yusuf <104871189+New101Leaf@users.noreply.github.com>
2024-07-11 18:19:33 -0400

    Update generator.c

--- Amusement Park Sim/generator.c
+++ Amusement Park Sim/generator.c
@@ -19,13 +19,13 @@ void main() {
   char buffer[50];
 
   for (int i=0; i<2; i++) {
-	printf(" \n NEW GUEST CREATED %d \n", i);
-    guestTickets = rand()%36 + 5;
-    guestWait = rand()%301 + 600;
-    priorityRide = rand()%NUM_RIDES;
-    sprintf(buffer, "./guest %d %d %d&", guestTickets, guestWait, priorityRide);
-   // printf("%s", buffer);
-    system(buffer);
+	  
+     printf(" \n NEW GUEST CREATED %d \n", i);
+     guestTickets = rand()%36 + 5;
+     guestWait = rand()%301 + 600;
+     priorityRide = rand()%NUM_RIDES;
+     sprintf(buffer, "./guest %d %d %d&", guestTickets, guestWait, priorityRide);
+     system(buffer);
 
     usleep(10000); // Keep this 1/100 second delay between guest creation
   }
